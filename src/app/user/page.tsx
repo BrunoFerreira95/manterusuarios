ee08131adfaac482f029db6249f910c498761766
Bruno Ferreira <brunoferreirapedraca@gmail.com>
2023-08-28 20:55:29 -0300

    style: pre-modeling of page

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -3,47 +3,48 @@ import React from 'react'
 const UserHome = () => {
   return (
     <>
-    <div className='flex justify-center min-h-screen max-h-fit'>
-
-      <header>
-        <h1>Gerenciamento de usuários</h1>
-      </header>
-      <hr />
-      <input type="text" placeholder='Pesquisar por nome'/>
-      <button> Adicionar Usuário</button>
-      <main>
-        <form action="">
-          <table>
-            <caption>Usuários</caption>
-            <thead>
-              <tr>
-                <th scope="col">Nome</th>
-                <th scope="col">Email</th>
-                <th scope="col">Data de Nascimento</th>
-                <th scope="col">Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th>Bruno</th>
-                <th>bruno@gmail.com</th>
-                <th>04/04/2004</th>
-                <th>Editar | Remover</th>
-              </tr>
-              <tr>
-                <th>Bruno</th>
-                <th>bruno@gmail.com</th>
-                <th>04/04/2004</th>
-                <th>Editar | Remover</th>
-              </tr>
-            </tbody>
-          </table>
-        </form>
-      </main>
-      <hr />
-      <footer>
-        <span>Desenvolvido por Bruno Ferreira</span>
-      </footer>
+    <div className='flex justify-center min-h-screen max-h-fit w-screen'>
+      <div className='flex flex-col w-2/4'>
+        <header className='h-20'>
+          <h1>Gerenciamento de usuários</h1>
+        </header>
+        <hr />
+        <main>
+          <input type="text" placeholder='Pesquisar por nome'/>
+          <button> Adicionar Usuário</button>
+          <form action="">
+            <table>
+              <caption>Usuários</caption>
+              <thead>
+                <tr>
+                  <th scope="col">Nome</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Data de Nascimento</th>
+                  <th scope="col">Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Bruno</th>
+                  <th>bruno@gmail.com</th>
+                  <th>04/04/2004</th>
+                  <th>Editar | Remover</th>
+                </tr>
+                <tr>
+                  <th>Bruno</th>
+                  <th>bruno@gmail.com</th>
+                  <th>04/04/2004</th>
+                  <th>Editar | Remover</th>
+                </tr>
+              </tbody>
+            </table>
+          </form>
+        </main>
+        <hr />
+        <footer>
+          <span>Desenvolvido por Bruno Ferreira</span>
+        </footer>
+      </div>
     </div>
     </>
   )
