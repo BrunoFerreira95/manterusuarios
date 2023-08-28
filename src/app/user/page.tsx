32b2c4d02e765e961f1875fe0a8dbc6f3ae0af00
Bruno Ferreira <brunoferreirapedraca@gmail.com>
2023-08-28 20:43:52 -0300

    feat: user: adding main with a form

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -7,6 +7,35 @@ const UserHome = () => {
       <h1>Gerenciamento de usuários</h1>
     </header>
       <hr />
+      <main>
+        <form action="">
+          <table>
+            <caption>Usuários</caption>
+            <thead>
+              <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Email</th>
+                <th scope="col">Data de Nascimento</th>
+                <th scope="col">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>Bruno</th>
+                <th>bruno@gmail.com</th>
+                <th>04/04/2004</th>
+                <th>Editar | Remover</th>
+              </tr>
+              <tr>
+                <th>Bruno</th>
+                <th>bruno@gmail.com</th>
+                <th>04/04/2004</th>
+                <th>Editar | Remover</th>
+              </tr>
+            </tbody>
+          </table>
+        </form>
+      </main>
       <hr />
     <footer>
       <span>Desenvolvido por Bruno Ferreira</span>
